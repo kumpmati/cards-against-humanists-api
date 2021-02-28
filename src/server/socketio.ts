4336f909053dc44b0b9119bf257b77ee83675ba4
mvkump <mvkump@utu.fi>
2021-02-28 17:49:09 +0200

    opened up socket.io CORS

--- src/server/socketio.ts
+++ src/server/socketio.ts
@@ -1,12 +1,19 @@
 import { Server } from "socket.io";
 import { Config } from "../config/config";
 
+const SOCKETIO_OPTS = {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+};
+
 /**
  * Starts socket.io on the port specified in config
  * @param config Config
  */
 export const startSocketIO = (config: Config): Server => {
-  const io = new Server(config.port);
+  const io = new Server(config.port, SOCKETIO_OPTS);
   console.log("socket.io started on port", config.port);
   return io;
 };
