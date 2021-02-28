5a6150aa0c4c74bd9aa3c5f99316330c2fd576f8
mvkump <mvkump@utu.fi>
2021-02-28 17:52:49 +0200

    UserController features - Event is now triggered whenever socket sends an event to the server - Added function "Send" to send events to client

--- src/controller/user/user.ts
+++ src/controller/user/user.ts
@@ -1,16 +1,22 @@
 import { Socket } from "socket.io";
 import { Event, TypedEvent } from "../../event";
+import { JoinGameEvent, LeaveGameEvent } from "./types";
 
-export class UserController<E, T> {
+export class UserController<E extends string, T> {
   private socket: Socket;
 
-  private readonly onJoinGame = new Event<any>();
-  private readonly onLeaveGame = new Event<any>();
+  private readonly onJoinGame = new Event<JoinGameEvent>();
+  private readonly onLeaveGame = new Event<LeaveGameEvent>();
   private readonly onEvent = new TypedEvent<E, T>();
 
   constructor(s: Socket) {
     this.socket = s;
+    this.socket.onAny(this.onEvent.trigger);
   }
+
+  public Send = (event: E, data: T) => {
+    this.socket.emit(event, data);
+  };
 
   public get JoinGame() {
     return this.onJoinGame.expose();
