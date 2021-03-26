fd32d2dde60bc812f5382ad58caeb2c97fbb328d
mvkump <mvkump@utu.fi>
2021-03-26 23:52:19 +0200

    Added firebase to store game state

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
+import admin from 'firebase-admin';
+import { Firestore } from "bgio-firebase";
 import { Server } from "boardgame.io/server";
 import { v4 } from "uuid";
 import { DEFAULT_CONFIG } from "../OLD/config";
 import { Cahum } from "./game";
 
+const database = new Firestore({
+  config: {
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'https://cahum-2f40d.firebaseio.com',
+  },
+  dbPrefix: "cahum_"
+});
+
 const server = Server({
   games: [Cahum],
   uuid: () => v4().slice(0, 5),
+  db: database
 });
+
 server.run(DEFAULT_CONFIG.port);
