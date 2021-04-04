5ae4ad1c2513250ae5259faded2055a7dfde4766
mvkump <mvkump@utu.fi>
2021-04-04 20:10:11 +0300

    startGame doc

--- src/game/phases/waitForPlayers/moves.ts
+++ src/game/phases/waitForPlayers/moves.ts
@@ -1,5 +1,11 @@
 import { Ctx } from "boardgame.io";
 
-export const startGame = (G: any, ctx: Ctx, ...args: any[]) => {
+/**
+ * Starts the game by going to the 'play' phase
+ * @param G
+ * @param ctx
+ * @param args
+ */
+export const startGame = (G: any, ctx: Ctx) => {
   ctx.events.setPhase("play");
 };
