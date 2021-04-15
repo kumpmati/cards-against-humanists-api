f2d102f232bec56b15955531a19438768c0e9fb2
mvkump <mvkump@utu.fi>
2021-04-16 00:29:28 +0300

    Added hybrid connector to development mode

---
+++ src/db/connectors/hybrid.ts
@@ -0,0 +1,70 @@
+import { Config } from "../../config/config";
+import { Card, CardPack } from "../../game/types";
+import { DBConnector, DBConnectorRequest } from "../types";
+import { DiskConnector } from "./disk";
+import { FirebaseConnector } from "./firebase";
+
+/**
+ * Hybrid connector. Loads all cards initially from disk,
+ * then any updates from Firestore will be updated to the disk
+ */
+export class HybridConnector implements DBConnector {
+  private firebase: FirebaseConnector;
+  private disk: DiskConnector;
+
+  async init(config: Config) {
+    console.log("[Hybrid] - Initializing...");
+
+    this.firebase = new FirebaseConnector();
+    await this.firebase.init(config);
+
+    this.disk = new DiskConnector();
+    await this.disk.init(config);
+
+    console.log("[Hybrid] - Initialized");
+  }
+
+  /**
+   * TODO: get
+   * Returns all cards of given type in a card pack
+   * @param opts
+   * @returns
+   */
+  async get<T extends Card>(opts: DBConnectorRequest): Promise<T[]> {
+    return this.firebase.get(opts);
+  }
+
+  /**
+   * Returns all the card packs on disk
+   * @returns
+   */
+  async getAll(): Promise<CardPack[]> {
+    console.log(" -- Loading from disk...");
+    return this.disk.getAll();
+  }
+
+  /**
+   * TODO: add
+   * Writes a card to the disk
+   * @param card
+   * @returns
+   */
+  async add(card: Omit<Card, "id">): Promise<string> {
+    return this.disk.add(card);
+  }
+
+  /**
+   * TODO: attachListeners
+   */
+  async attachListeners(...args: any[]) {}
+
+  /**
+   * TODO: detachListeners
+   */
+  async detachListeners() {}
+
+  /**
+   * TODO: disconnect
+   */
+  async disconnect() {}
+}
--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@ import { Cahum } from "./game";
 import { DEFAULT_CONFIG } from "./config";
 import { attachApiRoutes } from "./api";
 import { DB } from "./db";
-import { DiskConnector } from "./db/connectors/disk";
 import { FirebaseConnector } from "./db/connectors/firebase";
+import { HybridConnector } from "./db/connectors/hybrid";
 
 const uuid = () => v4().slice(0, 5);
 
 const start = async () => {
   // use disk when in development
-  DB.use(DEFAULT_CONFIG.dev ? new DiskConnector() : new FirebaseConnector());
+  DB.use(DEFAULT_CONFIG.dev ? new HybridConnector() : new FirebaseConnector());
   await DB.init(DEFAULT_CONFIG);
 
   const server = Server({
