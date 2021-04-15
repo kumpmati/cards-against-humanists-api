2e101700c7d37b87196d91d171f27094a4aa6c77
mvkump <mvkump@utu.fi>
2021-04-16 00:00:41 +0300

    Added disk connector

--- src/db/connectors/disk.ts
+++ src/db/connectors/disk.ts
@@ -1,5 +1,7 @@
 import { readFile } from "fs/promises";
-import { CardPack } from "../../game/types";
+import { Config } from "../../config/config";
+import { Card, CardPack } from "../../game/types";
+import { DBConnector, DBConnectorRequest } from "../types";
 
 export const loadCardsFromDisk = async (path: string) => {
   const rawData = (await readFile(path)).toString();
@@ -11,3 +13,64 @@ export const loadCardsFromDisk = async (path: string) => {
     );
   return data.packs as CardPack[];
 };
+
+/**
+ * Disk DBConnector.
+ * Writes and reads card data from disk.
+ */
+export class DiskConnector implements DBConnector {
+  private path: string;
+
+  async init(config: Config) {
+    console.log("[Disk] - Initializing...");
+
+    this.path = process.env.JSON_DB_PATH ?? "./dev/cards.json";
+
+    console.log("[Disk] - Initialized");
+  }
+
+  async get<T extends Card>(opts: DBConnectorRequest): Promise<T[]> {
+    return [];
+  }
+
+  /**
+   * TODO: add
+   * Writes a card to the disk
+   * @param card
+   * @returns
+   */
+  async add(card: Omit<Card, "id">): Promise<string> {
+    return null;
+  }
+
+  /**
+   * Returns all the card packs on disk
+   * @returns
+   */
+  async getAll(): Promise<CardPack[]> {
+    const rawData = (await readFile(this.path)).toString();
+    const data = JSON.parse(rawData);
+
+    if (!data.packs)
+      throw new Error(
+        "Invalid file format. JSON should contain key 'packs' of type CardPack[]"
+      );
+
+    return data.packs as CardPack[];
+  }
+
+  /**
+   * TODO: attachListeners
+   */
+  async attachListeners(...args: any[]) {}
+
+  /**
+   * TODO: detachListeners
+   */
+  async detachListeners() {}
+
+  /**
+   * TODO: disconnect
+   */
+  async disconnect() {}
+}
--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import { Cahum } from "./game";
 import { DEFAULT_CONFIG } from "./config";
 import { attachApiRoutes } from "./api";
 import { DB } from "./db";
+import { DiskConnector } from "./db/connectors/disk";
 import { FirebaseConnector } from "./db/connectors/firebase";
 
 const uuid = () => v4().slice(0, 5);
 
 const start = async () => {
-  DB.use(new FirebaseConnector());
+  // use disk when in development
+  DB.use(DEFAULT_CONFIG.dev ? new DiskConnector() : new FirebaseConnector());
   await DB.init(DEFAULT_CONFIG);
 
   const server = Server({
